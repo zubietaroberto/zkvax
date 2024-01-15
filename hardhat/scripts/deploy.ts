502ce44361f1e2e8a51af6cbf2fbc7dfee74014f
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2024-01-15 18:14:58 -0500

    Hardhat deployment working

--- hardhat/scripts/deploy.ts
+++ hardhat/scripts/deploy.ts
@@ -1,26 +1,22 @@
 import { ethers } from "hardhat";
 
+const merkleRoot =
+  "0x29fd5ee89e33f559a7b32ac39f57400aa5a6c77492e28c088f9eb511b0c73e78";
+const nullifierHash =
+  "0x1cbb284a43dde14da2c3790e12872bcb7e53c53e27b5187384c617841174ace5";
+
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const verifierContract = await ethers.getContractFactory("UltraVerifier");
+  const verifier = await verifierContract.deploy();
+  const verifierAddress = await verifier.getAddress();
+  console.log(`Verifier delpoyed to: ${verifierAddress}`);
 
-  const lockedAmount = ethers.parseEther("0.001");
-
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  const votingContract = await ethers.getContractFactory("Voting");
+  const voting = await votingContract.deploy(merkleRoot, verifierAddress);
+  const votingAddress = await voting.getAddress();
+  console.log(`Voting delpoyed to: ${votingAddress}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
