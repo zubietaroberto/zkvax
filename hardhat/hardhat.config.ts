3e863fcde4ba85970dc37cdac0165cacb4f1ae1a
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2024-01-15 17:49:13 -0500

    compilation working

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -2,7 +2,20 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.8.23",
+
+    // Note: Optimizer is required or we will get a "stack too deep" error
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 10000,
+        details: {
+          yul: true,
+        },
+      },
+    },
+  },
 };
 
 export default config;
