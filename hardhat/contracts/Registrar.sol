7940692e10320f3b134c694bee5a27d84a5ddc01
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2024-01-22 10:52:56 -0500

    Added registration contract.

---
+++ hardhat/contracts/Registrar.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.23;
+
+import {UltraVerifier} from "../circuits/contract/zkvax/plonk_vk.sol";
+import "./MerkleRegistry.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+abstract contract Registrar is MerkleRegistry, ReentrancyGuard {
+    UltraVerifier verifier;
+
+    // we store all commitments to prevent duplicates
+    mapping(bytes32 => bool) public commitments;
+
+    event Registration(
+        bytes32 indexed commitment,
+        uint32 leafIndex,
+        uint256 timestamp
+    );
+
+    /**
+    @dev The constructor
+    @param _merkleTreeHeight the height of deposits' Merkle Tree
+    */
+    constructor(uint32 _merkleTreeHeight) MerkleRegistry(_merkleTreeHeight) {
+        verifier = new UltraVerifier();
+    }
+
+    /**
+    @dev Add a new commitment to the tree
+    @param _commitment the note commitment, which is PedersenHash(nullifier + secret)
+    */
+    function register(bytes32 _commitment) external payable nonReentrant {
+        require(!commitments[_commitment], "The commitment has been submitted");
+
+        uint32 insertedIndex = _insert(_commitment);
+        commitments[_commitment] = true;
+
+        emit Registration(_commitment, insertedIndex, block.timestamp);
+    }
+
+    /**
+    @dev Check whether a leaf exists in the tree
+    */
+    function check(
+        bytes calldata _proof,
+        bytes32 _root,
+        bytes32 _nullifierHash
+    ) external payable nonReentrant returns (bool) {
+        require(isKnownRoot(_root), "Cannot find your merkle root"); // Make sure to use a recent one
+        bytes32[] memory inputs = new bytes32[](2);
+        inputs[0] = _root;
+        inputs[1] = _nullifierHash;
+        require(verifier.verify(_proof, inputs), "Invalid proof");
+
+        return true;
+    }
+}
