90363b72c61638ed8564ddb2077f04be87fbf00c
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2024-01-22 11:39:05 -0500

    Fixed deployment

--- hardhat/scripts/deploy.ts
+++ hardhat/scripts/deploy.ts
@@ -2,7 +2,7 @@ import { ethers } from "hardhat";
 
 async function main() {
   const registryContract = await ethers.getContractFactory("Registrar");
-  const registry = await registryContract.deploy(32);
+  const registry = await registryContract.deploy(31);
   const registryAddress = await registry.getAddress();
   console.log(`Registry delpoyed to: ${registryAddress}`);
 }
